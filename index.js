72d01583648c85dea583061787b77c3e9c717456
Christian Lopez <me@ralkage.com>
2022-12-22 22:03:22 -0500

    Add query string params support

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const port = 4444
 app.use(express.json())
 
 app.post("/", (req, res) => {
-  res.json({ requestHeaders: req.headers, requestBody: req.body })
+  res.json({
+    requestHeaders: req.headers,
+    requestBody: req.body,
+    requestParams: req.query,
+  })
 })
 
 app.listen(port, () => {
